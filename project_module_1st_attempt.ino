9878ba3eb6fba1c0a8d0ff5b2279fa12874e0b83
Natan Muntean <munteanaty@gmail.com>
2020-01-13 15:53:47 +0200

    Add files via upload

--- project_module_1st_attempt.ino
+++ project_module_1st_attempt.ino
@@ -15,6 +15,7 @@ int prev_x = -1;
 int prev_s = -1;
 
 int initial = 1;
+int readed = 0;
 int gameOn = 1;
 int direct = 1;
 int line = 0;
@@ -26,8 +27,10 @@ int next_x = 0;
 int next_s = 0;
 
 char block[8];
-
+int enable = 0;
 void setup() {
+  
+  Serial.begin(9600);
   Wire.begin(9);
   Wire.onReceive(receiveEvent);
   
@@ -35,7 +38,7 @@ void setup() {
   lcd.clear();
   
   for(int i = 0; i < 8; i++)
-      block[i] = 'z'+145;
+      block[i] = 'z'+144;
   pinMode(OUT_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -43,21 +46,24 @@ void setup() {
   digitalWrite(OUT_PIN, LOW);
   ledValue = 0;
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonInterrupt, FALLING);
+  enable = digitalRead(IN_PIN);
 
 }
 
 void loop() {
-  int enable = digitalRead(IN_PIN);
-  if(enable == 1){
+  if(enable == 1 || readed == 1){
     if (initial == 1){
-      if(prev_x != -1 && prev_s != -1){
+       buttonValue = 0;
+      if(readed == 1){
+        Serial.print("Was here!");
         x = prev_x;
-        y = prev_y;
+        s = prev_s;
         dlay = 50;
       }
       else
         dlay = 200;      
       initial = 0;
+      line = 0;
     }
     if(buttonValue == 0 && gameOn == 1){
       lcd.setCursor(x, line);
@@ -73,15 +79,17 @@ void loop() {
       lcd.print(block + s);
       delay(dlay);
     }
-    else if (gameOn == 1)
+    else if (buttonValue == 1 && gameOn == 1)
     {
+      
+      buttonValue = 0;
       if(line == 0)
       {
         dlay = 100;
         line = 1;
         if(prev_x != -1 && prev_s != -1){
           if(x != prev_x){
-            if(x + (8-s) < prev_x || prev_x + (8-prev_s) < x){
+            if(x + (8-s) <= prev_x || prev_x + (8-prev_s) <= x){
               lcd.setCursor(x, line);
               lcd.print("Game Over!");
               gameOn = 0;
@@ -97,29 +105,38 @@ void loop() {
           }
         }
       }
-      if(line == 1)
+      else
       {
         gameOn = 0;
-        if(prev_x != -1 && prev_s != -1){
-          if(x != prev_x){
-            if(x + (8-s) < prev_x || prev_x + (8-prev_s) < x){
-              lcd.setCursor(x, line);
-              lcd.print("Game Over!");
-              gameOn = 0;
-            }
-            else{
-              if(x > prev_x)
-                s = s + x - prev_x;
-              else
-                s = s + prev_x - x;
-            }
+        if(x != prev_x){
+          if(x + (8-s) <= prev_x || prev_x + (8-prev_s) <= x){
+            lcd.setCursor(0, line);
+            lcd.print("                ");
+            Serial.print("Game Over!");
+            lcd.setCursor(0, line);
+            lcd.print("Game Over!");
+            gameOn = 0;
+          }
+          else{
+            if(x > prev_x)
+              s = s + x - prev_x -2;
+            else
+              s = s + prev_x - x -2;
             next_x = x;
             next_s = s;
+            
+            Serial.print("Game On!");
+            
+            sendEvent();
+            delay(1000);
+            digitalWrite(OUT_PIN, HIGH);
+            lcd.setCursor(0, line);
+            lcd.print("                ");
+            lcd.setCursor(x, line);
+            lcd.print(block + s);
           }
         }
-        sendEvent();
       }
-      buttonValue = 0;
     }
   }
 }
@@ -129,15 +146,14 @@ void buttonInterrupt()
     buttonValue = 1;
 }
 void receiveEvent(int bytes) {
-  int val_x_s = Wire.read();
-  prev_s = val_x_s & 15;
-  prev_x = val_x_s >> 4;
+  prev_s = Wire.read(); //val_x_s & 15;
+  prev_x = Wire.read(); //val_x_s >> 4;
+  readed = 1;
 }
 void sendEvent(){
-  int val_x_s = next_x << 4 + next_s & 15;
+  int val_x_s_2 = next_x << 4 + next_s & 15;
   Wire.beginTransmission(9);
-  Wire.write(val_x_s);
+  Wire.write(next_s);
+  Wire.write(next_x);
   Wire.endTransmission(); 
-  
-  digitalWrite(OUT_PIN, HIGH);
 }
